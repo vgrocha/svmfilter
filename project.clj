25d0a70589c2c057b8d0c84e45c79336fdac8c46
Vinicius Rocha <vinicius.darocha@gmail.com>
2012-04-02 17:41:06 -0300

    found the hasExtension bug

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject recognizerTrainer "0.1.0-SNAPSHOT"
   :description "Training a recognizer"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"] [org.clojure/clojure-contrib "1.2.0"]])
--- src/recognizerTrainer/core.clj
+++ src/recognizerTrainer/core.clj
@@ -1,12 +1,24 @@
-(ns recognizerTrainer.core
-  (:import (java.io File)))
+;well, i want to link up the whole thing to our current decaptcher
+                                        ;framework
+;maybe check mr. bellanis sources =)
 
-(def folder (File. "/home/usuario/workspace/samples/trunk/captchas/detranPr"))
+(ns recognizerTrainer.core
+  (:import (java.io File)
+           (java.awt.image BufferedImage)
+           (javax.imageio ImageIO)))
+
+(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
+
+(def folder2 (File. "/home/usuario/workspace/samples/trunk/captchas/detranPr"))
+
+(def folder (File. "/home/vgrocha/tmp/cap"))
 
 (def ext "jpg")
 
 (defn get-files-from-folder [filterfn path]
   (filter filterfn (.listFiles path)))
+
+
 
 (defn File-has-extension [ext F]
   (let [name (.getName F)
@@ -14,9 +26,15 @@
         next (count ext)
         start (- nF next)]
     (if (<= 0 start)
-      (= (subs (.getName F) start nF) ext))
-    false
+      (= (subs (.getName F) start nF) ext)
+      false)
     ))
+
+(defn load-image [path]
+  (try
+    (ImageIO/read path)
+    (catch Exception e (.printStackTrace e))))
+
 
 (defn file-image? [f]
   (File-has-extension ext f))
